f1504025ea371baa2a7ac055f91fb1847904e2b7
unknown <salmanaidara01@gmail.com>
2023-06-19 16:03:20 +0000

    ajout csrf_token dans qr_scanner de ajax

--- scanpointage/settings.py
+++ scanpointage/settings.py
@@ -15,18 +15,6 @@ import dj_database_url
 import environ
 import os
 
-# url = os.environ.get('DATABASES_URL')
-# if url:
-#     DATABASES = {
-#         'default': dj_database_url.parse(url)
-#     }
-# else:
-#     # Gérer le cas où l'URL de la base de données est manquante ou invalide
-#     DATABASES = {}
-
-
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
